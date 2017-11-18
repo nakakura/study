915b1dc3a8ceec797ccbeb216a320f34b56e473f
Toshiya Nakakura <nakakura@me.com>
2017-11-18 14:49:52 +0900

    pure future & stream implementation

--- frp/future-stream/total_test/src/main.rs
+++ frp/future-stream/total_test/src/main.rs
@@ -41,102 +41,126 @@ fn make_post(command: String) -> SlackPost {
     }
 }
 
-use std::sync::RwLock;
+use std::io;
 
-#[derive(Debug)]
-struct Latest {
-    pub output: RwLock<Option<Vec<usize>>>,
+#[derive(Debug, PartialOrd, PartialEq, Clone)]
+struct OutputData {
+    pub message_array_opt: Option<Vec<usize>>,
+    pub position_opt: Option<usize>,
 }
 
-impl Latest {
-    pub fn set_latest(&self, message: Vec<usize>) {
-        *(self.output.write().unwrap()) = Some(message);
+impl OutputData {
+    pub fn can_output(&self) -> bool {
+        self.message_array_opt.is_some() && self.position_opt.is_some()
     }
 
-    pub fn erase_latest(&self) {
-        *(self.output.write().unwrap()) = None;
-    }
-
-    pub fn message(&self, pos: usize) -> usize {
-        if let Some(ref x) = *(self.output.read().unwrap()) {
-            println!("from {:?}", x);
-            if x.len() > pos {
-                x[pos]
-            } else {
-                println!("index out of range");
-                0
-            }
-        } else {
-            0
+    pub fn output(&self) {
+        if let (&Some(ref array), &Some(ref position)) = (&self.message_array_opt, &self.position_opt) {
+            let x = array.get(*position);
+            x.map(|data| {
+                println!("###########\n{:?}\n#############", data);
+            });
         }
     }
 
-    pub fn flag(&self) -> bool {
-        self.output.read().unwrap().is_some()
+    pub fn is_data(&self) -> bool {
+        self.message_array_opt.is_some()
+    }
+
+    pub fn feed(&self, data: OutputData) {
+        OutputData {
+            message_array_opt: self.message_array_opt.clone(),
+            position_opt: data.position_opt.clone()
+        }.output();
+    }
+}
+
+impl Future for OutputData {
+    type Item = OutputData;
+    type Error = io::Error;
+
+    fn poll(&mut self) -> Poll<OutputData, io::Error> {
+        let outputdata = OutputData {
+            message_array_opt: None,
+            position_opt: None,
+        };
+
+        Ok(Async::Ready(outputdata))
+    }
+}
+
+fn db2outputdata(db: Vec<usize>) -> OutputData {
+    if db.len() == 1 {
+        OutputData {
+            message_array_opt: Some(db),
+            position_opt: Some(0),
+        }
+    } else {
+        OutputData {
+            message_array_opt: Some(db),
+            position_opt: None,
+        }
     }
 }
 
 fn set_receiver(receiver: mpsc::Receiver<String>) {
-    let _ = thread::spawn(move || {
-        let (mut tx_post, rx_post) = mpsc::channel::<SlackPost>(5000);
-        let (mut tx_num, rx_num) = mpsc::channel::<usize>(5000);
+    let (mut tx_output, rx_output) = mpsc::channel::<OutputData>(5000);
+    let (mut tx_slackpost, rx_slackpost) = mpsc::channel::<SlackPost>(5000);
 
-        let th = thread::spawn(move || {
-            let mut core = Core::new().unwrap();
+    let th_parse_input_message = thread::spawn(move || {
+        let mut core = Core::new().unwrap();
 
-            let x = receiver.map(make_post).fold((tx_post, tx_num), |(s_p, s_n), x| {
-                let num = x.command.parse::<usize>();
-                if let Ok(val) = num {
-                    Ok((s_p, s_n.send(val).wait().unwrap()))
-                } else {
-                    Ok((s_p.send(x).wait().unwrap(), s_n))
-                }
-            });
-            let _ = core.run(x);
+        let x = receiver.map(make_post).fold((tx_output, tx_slackpost), |tx, post| {
+            let num = post.command.parse::<usize>();
+            if let Ok(val) = num {
+                let output = tx.0.send(OutputData {
+                    message_array_opt: None,
+                    position_opt: Some(val),
+                }).wait().unwrap();
+                Ok((output, tx.1))
+            } else {
+                let slackpost = tx.1.send(post).wait().unwrap();
+                Ok((tx.0, slackpost))
+            }
         });
-
-        use std::sync::Arc;
-        let latest = Arc::new(Latest{output: RwLock::new(None)});
-
-        let l1 = latest.clone();
-        let th_post = thread::spawn(move || {
-            let mut core = Core::new().unwrap();
-            let x = rx_post.map(search_db).for_each(|x|{
-                if x.len() == 1 {
-                    l1.erase_latest();
-                    println!("#########{}############", x[0]);
-                } else {
-                    println!("reset & wait");
-                    l1.set_latest(x);
-                }
-                Ok(())
-            });
-            let _ = core.run(x);
-        });
-
-        let l = latest.clone();
-        let th_num = thread::spawn(move || {
-            let mut core = Core::new().unwrap();
-            let x = rx_num.filter(|_x| {
-                let flag = l.flag();
-                flag
-            }).for_each(|x|{
-                println!("#############{:?}###########", l.message(x));
-                l.erase_latest();
-                Ok(())
-            });
-            let _ = core.run(x);
-        });
-
-        let _ = th.join();
-        let _ = th_post.join();
-        let _ = th_num.join();
+        let _ = core.run(x);
     });
+
+    let rx_message_array = rx_slackpost.map(search_db).map(db2outputdata);
+
+    let rx_merge = rx_output.select(rx_message_array);
+    let th_output = thread::spawn(move || {
+        let mut core = Core::new().unwrap();
+        let data = OutputData{ message_array_opt: None, position_opt: None };
+        let x = rx_merge.fold(data.clone(), |prev, x| {
+            if x.can_output() {
+                x.output();
+                Ok(data.clone())
+            } else {
+                if x.is_data() {
+                    println!("there are multiple target. please input position to specifiy");
+                    Ok(x)
+                } else if prev.is_data() {
+                    prev.feed(x);
+                    Ok(data.clone())
+                } else {
+                    Ok(data.clone())
+                }
+            }
+        });
+
+        let _ = core.run(x);
+    });
+
+    let _ = th_parse_input_message.join();
+    let _ = th_output.join();
 }
 
 fn main() {
     let (mut tx, rx) = mpsc::channel::<String>(5000);
-    set_receiver(rx);
+    let th = thread::spawn(move || {
+        set_receiver(rx);
+    });
 
     loop {
         println!(">mono SUBCOMMAND");
@@ -149,4 +173,6 @@ fn main() {
             tx = tx.send(line.to_string()).wait().unwrap();
         }
     }
+
+    let _ = th.join();
 }
