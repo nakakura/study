48f0ca1bafc9a350d286a03b0263861add6d8334
Toshiya Nakakura <nakakura@me.com>
2018-05-18 12:47:34 +0900

    update futures-fs

--- rust/futures-fs-test/src/main.rs
+++ rust/futures-fs-test/src/main.rs
@@ -2,7 +2,12 @@ extern crate futures;
 extern crate futures_fs;
 extern crate bytes;
 extern crate tokio;
+#[macro_use]
+extern crate nom;
+extern crate either;
 
+use either::*;
+use nom::*;
 use futures::*;
 use futures::sync::mpsc;
 use futures_fs::FsPool;
@@ -16,25 +21,38 @@ fn main() {
     let fs = FsPool::default();
 
 // our source file
-    let read = fs.read("~/pingcopy.txt", Default::default());
+    let read = fs.read("/home/nakakura/pingcopy.txt", Default::default());
 
 // default writes options to create a new file
-    let mut write = fs.write("~/pingcopy.txt", Default::default());
+    let mut write = fs.write("/home/nakakura/pingcopy.txt", Default::default());
 
 // block this thread!
 // the reading and writing however will happen off-thread
 
     let (tx, rx) = mpsc::channel::<Bytes>(1500);
-    for _x in 0..100000 {
+    /*
+    let mut buf = BytesMut::with_capacity(1024);
+    buf.put_u16_be(10u16);
+    buf.put(vec!(0, 1, 2, 3, 4, 5, 6, 7, 8));
+    write = write.send(buf.freeze()).wait().unwrap();
+    */
+    /*
+    for _x in 0..10000 {
         let mut buf = BytesMut::with_capacity(1024);
         buf.put_u16_be(10u16);
         buf.put(vec!(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
         write = write.send(buf.freeze()).wait().unwrap();
     }
+    */
     let mut buf = BytesMut::with_capacity(999999);
     let r = read.map_err(|_| ()).fold((tx, buf), |mut sum: (mpsc::Sender<Bytes>, BytesMut), x| {
-        let tx = sum.0.send(x).wait().unwrap();
-        Ok((tx, sum.1))
+        use nom::IResult::Done;
+        sum.1.put(x);
+        let r = e(&sum.1);
+        let mut buf = BytesMut::with_capacity(999999);
+        buf.put(r);
+
+        Ok((sum.0, buf))
     }).map(|x| ());
 
     use std::thread;
@@ -42,7 +60,6 @@ fn main() {
 
     let th = thread::spawn(|| {
         let rx = rx.for_each(|x| {
-            println!("{:?}", x);
             Ok(())
         });
         tokio::run(rx);
@@ -58,3 +75,42 @@ fn main() {
 
     let _ = th.join();
 }
+
+fn e(i: &[u8]) -> &[u8] {
+    let x = ex(i);
+    match x {
+        Left(l) => {
+            e(l.1)
+        },
+        Right(r) => {
+            println!("right {:?}", r);
+            r
+        }
+    }
+}
+
+fn ex(i: &[u8]) -> Either<(&[u8], &[u8]), &[u8]>{
+    let x = extract(i);
+    if x.is_done() {
+        let x = x.unwrap().1;
+        Left((x.1, x.2))
+    } else if x.is_incomplete() {
+        Right(i)
+    } else {
+        println!("err");
+        unreachable!();
+    }
+}
+
+fn extract(i: &[u8]) -> IResult<&[u8], (u16, &[u8], &[u8])> {
+    let total_len = i.len();
+    do_parse!(i,
+                data_len: be_u16
+            >>  payload: take!(data_len)
+            >>  rest: take!(total_len - data_len as usize - 2)
+            >> (
+                data_len, payload, rest
+            )
+        )
+}
+
